27b628335d2a4136e83774e62ce7188b8cdd5991
Paweł Kobryński <pawelkobrynski97@gmail.com>
2019-10-17 23:48:28 +0200

    Dodanie tworzenia kursu

---
+++ API/Security/AuthorizeRolesAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+
+namespace API.Security
+{
+    public class AuthorizeRolesAttribute : AuthorizeAttribute
+    {
+        public AuthorizeRolesAttribute(params string[] roles) : base()
+        {
+            Roles = string.Join(",", roles);
+        }
+    }
+}
---
+++ Application/Courses/Create.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Errors;
+using Application.Interfaces;
+using Domain;
+using FluentValidation;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace Application.Courses
+{
+    public class Create
+    {
+        public class Command : IRequest
+        {
+            public Guid Id { get; set; }
+            public string Name { get; set; }
+        }
+
+        public class CommandValidator : AbstractValidator<Command>
+        {
+            public CommandValidator()
+            {
+                RuleFor(x => x.Name).NotEmpty();
+            }
+        }
+
+        public class Handler : IRequestHandler<Command>
+        {
+            private readonly DataContext _context;
+            private readonly IUserAccessor _userAccessor;
+            public Handler(DataContext context, IUserAccessor userAccessor)
+            {
+                _userAccessor = userAccessor;
+                _context = context;
+            }
+
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+                if (await _context.Courses.Where(x => x.Name == request.Name).AnyAsync())
+                    throw new RestException(HttpStatusCode.BadRequest, new { Nazwa = "Kurs o takiej nazwie już istnieje" });
+
+                var course = new Course
+                {
+                    Id = request.Id,
+                    Name = request.Name
+                };
+
+                _context.Courses.Add(course);
+
+                
+                var success = await _context.SaveChangesAsync() > 0;
+
+                if (success) return Unit.Value;
+
+                throw new Exception("Problem podczas zapisu");
+            }
+        }
+    }
+}
