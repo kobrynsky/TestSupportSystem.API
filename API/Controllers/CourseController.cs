179ed68bf3dfeaa371f8daaca0002404564ffc2e
Paweł Kobryński <pawelkobrynski97@gmail.com>
2019-10-17 23:48:52 +0200

    Dodanie kontrolera do kursów

---
+++ API/Controllers/CourseController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Application.Courses;
+using Application.User;
+using API.Security;
+using Domain;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class CourseController: BaseController
+    {
+        [AuthorizeRoles(Role.Lecturer, Role.MainLecturer)]
+        [HttpPost]
+        public async Task<ActionResult<Unit>> Create(Create.Command command)
+        {
+            return await Mediator.Send(command);
+        }
+    }
+}
