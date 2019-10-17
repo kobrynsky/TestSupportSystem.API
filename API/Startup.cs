a5243b369cedb931a3827b7c5cf5c8db39d6ed5a
Paweł Kobryński <pawelkobrynski97@gmail.com>
2019-10-17 02:57:27 +0200

    Dodanie obsługi CORS, dodanie opcji używania autentykacji

--- API/Startup.cs
+++ API/Startup.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Application.Interfaces;
 using Application.User;
+using API.Middleware;
 using Domain;
 using Infrastructure.Security;
 using MediatR;
@@ -37,6 +38,13 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(o => o.AddPolicy("MyPolicy", policy =>
+            {
+                policy.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader();
+            }));
+
             services.AddDbContext<DataContext>(opt =>
             {
                 opt.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
@@ -64,24 +72,28 @@ namespace API
 
             services.AddMediatR(typeof(Login.Handler).Assembly);
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-            services.AddScoped<IJwtGenerator, JwtGenerator>();
             services.AddScoped<IUserAccessor, UserAccessor>();
+            services.AddScoped<IJwtGenerator, JwtGenerator>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseMiddleware<ErrorHandlingMiddleware>();
+
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+//                app.UseDeveloperExceptionPage();
             }
             else
             {
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-//                app.UseHsts();
+                //                app.UseHsts();
             }
 
-//            app.UseHttpsRedirection();
+            //            app.UseHttpsRedirection();
+            app.UseAuthentication();
+            app.UseCors("MyPolicy");
             app.UseMvc();
         }
     }
