7bc05e059076a732d36bf15a9a5b6c532be41e14
Paweł Kobryński <pawelkobrynski97@gmail.com>
2019-10-21 18:01:35 +0200

    Poprawienie metody group/list

--- Application/Groups/List.cs
+++ Application/Groups/List.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -44,7 +45,7 @@ namespace Application.Groups
 
             public async Task<List<GroupDto>> Handle(List.Query request, CancellationToken cancellationToken)
             {
-                var groups = await _context.Groups.ToListAsync(cancellationToken);
+                var groups = await _context.Groups.Include(x => x.UserGroups).ThenInclude(y => y.User).ToListAsync();
                 return _mapper.Map<List<GroupDto>>(groups);
             }
         }
