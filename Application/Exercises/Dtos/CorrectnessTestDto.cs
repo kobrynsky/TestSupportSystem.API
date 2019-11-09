cd54aea75545842888639d6778055246483021c9
Paweł Kobryński <pawelkobrynski97@gmail.com>
2019-11-09 22:32:53 +0100

    Poprawka nazewnictwa

--- Application/Exercises/Dtos/CorrectnessTestDto.cs
+++ Application/Exercises/Dtos/CorrectnessTestDto.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 
 namespace Application.Exercises.Dtos
 {
-    public class CorrectnessTestsDto
+    public class CorrectnessTestDto
     {
         public List<string> Outputs { get; set; }
         public List<string> Inputs { get; set; }
